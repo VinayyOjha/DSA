3b96739defdb708363616b0ffc6737c8041741d9
VinayyOjha <ojhavinay7602@.com>
2025-08-05 10:36:47 +0530

    bubble sort

---
+++ Striver/04 Sorting/BubbleSort.java
@@ -0,0 +1,24 @@
+
+import java.util.Arrays;
+
+
+public class BubbleSort{
+    public static void main(String args[]){
+        int arr[] = {9, 8, 1, 4, 6, 54, -100};
+        sortArray(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void sortArray(int[] arr) {
+        for(int i = 0; i < arr.length; i++){
+            for(int j = i; j < arr.length - 1; j++){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }   
+}
+
